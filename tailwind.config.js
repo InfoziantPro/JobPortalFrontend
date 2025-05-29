b7bc60bd6a5e01a720ff848a5ce264891209dfe6
InfoziantPro <infoziantproject@gmail.com>
2025-05-29 08:26:48 +0530

    Completed Home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,22 @@ export default {
           '0%': { transform: 'translateX(100%)' },
           '100%': { transform: 'translateX(-100%)' },
         },
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
       },
       animation: {
         marquee: 'marquee 25s linear infinite',
+        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
       },
     },
   },
   plugins: [],
 }
- 
